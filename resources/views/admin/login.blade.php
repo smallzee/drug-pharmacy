2240138ade7d14615d4cd949c3b62bb10775343d
Tech4All <tech4all583@gmail.com>
2021-07-15 14:25:32 +0100

    initial

--- resources/views/admin/login.blade.php
+++ resources/views/admin/login.blade.php
@@ -16,6 +16,15 @@
     <link rel="stylesheet" href="{{url('assets/admin/css/style.css')}}">
     <link rel="stylesheet" href="{{url('assets/admin/css/skin_color.css')}}">
 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
+    <style>
+        .form-control{
+            height: 45px;
+        }
+    </style>
+
 </head>
 
 <body class="hold-transition theme-primary bg-img" style="background-image: url({{url('images/bg.jpg')}})" data-overlay="2">
@@ -41,7 +50,7 @@
                                 <div class="form-group">
                                     <div class="input-group mb-3">
                                         <div class="input-group-prepend">
-                                            <span class="input-group-text bg-transparent"><i class="ti-user"></i></span>
+                                            <span class="input-group-text bg-transparent"><i class="fa fa-user"></i></span>
                                         </div>
                                         <input type="email" name="email_address" required class="form-control pl-15 bg-transparent" placeholder="Email Address">
                                     </div>
@@ -49,7 +58,7 @@
                                 <div class="form-group">
                                     <div class="input-group mb-3">
                                         <div class="input-group-prepend">
-                                            <span class="input-group-text bg-transparent"><i class="ti-lock"></i></span>
+                                            <span class="input-group-text bg-transparent"><i class="fa fa-lock"></i></span>
                                         </div>
                                         <input type="password" name="password" required class="form-control pl-15 bg-transparent" placeholder="Password">
                                     </div>
@@ -63,7 +72,7 @@
                                     </div>
                                     <!-- /.col -->
                                     <div class="col-12 text-center">
-                                        <button type="submit" class="btn btn-danger btn-block mt-10">SIGN IN</button>
+                                        <button type="submit" class="btn btn-primary btn-block mt-10">SIGN IN</button>
                                     </div>
                                     <!-- /.col -->
                                 </div>
